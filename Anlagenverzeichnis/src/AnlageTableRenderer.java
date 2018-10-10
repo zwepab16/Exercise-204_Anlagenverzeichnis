3dbb80f5d1e7f94c952b291651437e68fe4fda82
zwepab16 <zwepab16@htlkaindorf.at>
2018-10-10 09:59:33 +0200

    Klassen und deren Variablen erstellt.

--- Anlagenverzeichnis/src/AnlageTableRenderer.java
+++ Anlagenverzeichnis/src/AnlageTableRenderer.java
@@ -1,5 +1,15 @@
 
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
 
-public class AnlageTableRenderer {
+
+
+public class AnlageTableRenderer implements TableCellRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(JTable jtable, Object o, boolean bln, boolean bln1, int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
